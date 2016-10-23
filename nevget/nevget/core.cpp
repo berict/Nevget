85c5b64ca05f062c63b6f4e0b04b1021cdc1a8fb
BedrockDev <dev.bedrock@gmail.com>
2016-10-24 00:13:41 +0900

    working algorithm cpp
    interval calculating cpp code

--- nevget/nevget/core.cpp
+++ nevget/nevget/core.cpp
@@ -1,45 +1,47 @@
 #include <stdio.h>
-#include <math.h>
 #include <conio.h>
+#include <math.h>
 
-static double POW = 2.42; 
-// change this for various user age
-// increase for longer interval, vice versa
+float x = 0, y = 0;
+float level = 1;
+float last_interval_date = 0;
+// https://www.desmos.com/calculator/lhwfudnxsj
 
-int today = 1;
-int level = 0;
-int created_date = 0;
-int last_interval_date = 0;
-int age = 20;
-double rate = 0;
-static double e = 2.71828182846;
-
-void sendEmail() {
+void sendEmail(int level, int x, int age)
+{
 	printf("==========================\n");
 	printf("Email sent : on level %d\n", level - 1);
-	printf("           : at day %d\n", today);
+	printf("           : at day %.3f\n", (x + last_interval_date));
 	printf("           : user age %d\n", age);
 	printf("==========================\n\n");
 }
 
-void interval() {
-	double date_passed = today - last_interval_date;
-	double rate = (double)(1 - pow(e, -1.0 * ((double)date_passed / (double)(1.0 * (pow(level, 2.42))))));
-	printf("Remember rate : %.3lf\n\n", rate);
-	if (0 <= 0.900) {
-		// under 90%, interval
-		level++;
-		last_interval_date = today;
-		sendEmail();
-	}
-}
+void main()
+{
+	int age = 20;
+	float e = 2.71828182;
 
-void main() {
-	printf("User age : %d\n\n", age);
-	while (1) {
-		printf("Day %d\n", today);
-		interval();
+	printf("Input any other key to exit\n\n");
+
+	while (age <= 0)
+	{
+		printf("Enter correct user age : ");
+		scanf("%d", &age);
+	}
+
+	while (1)
+	{
+		printf("time %.2f\n", (x + last_interval_date));
+		y = pow(e, -(x / (1.898 * (pow(level, 2)))));
+		printf("Forget y : %f / 1\n\n", y);
+		if (y <= 0.9)
+		{
+			level++;
+			last_interval_date += x;
+			x = 0;
+			sendEmail(level, x, age);
+		}
 		getch();
-		today++;
+		x += 0.1;
 	}
 }
