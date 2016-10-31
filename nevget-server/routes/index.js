4c29154888fb7570151e733d8372085eb16b4e2a
root <iwin12372@gmail.com>
2016-10-31 04:34:26 +0000

    start auth

---
+++ nevget-server/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/al', function(req, res){
+  res.render('algorithm');
+});
+
+module.exports = router;
