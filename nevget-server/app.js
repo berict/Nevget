ef50ded7d550e2c0bbef59f7b86995ddfa3055d4
root <iwin12372@gmail.com>
2016-11-02 01:52:32 +0000

    end email send  logic

--- nevget-server/app.js
+++ nevget-server/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mailer = require('express-mailer');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/ovs');
 
@@ -23,14 +24,16 @@ var reminder = new mongoose.Schema({
 
 Users = mongoose.model('users', UserSchema);
 
+var app = express();
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+require('./routes/mail')(app, mailer);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
